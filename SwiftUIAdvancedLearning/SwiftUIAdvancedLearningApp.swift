eda996255dc1aac6ac52677995f927d36a486e21
Fernando V <lvargasp@uni.pe>
2025-05-18 16:50:54 -0500

    Initial Commit

---
+++ SwiftUIAdvancedLearning/SwiftUIAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdvancedLearningApp.swift
+//  SwiftUIAdvancedLearning
+//
+//  Created by FerVP on 18/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
